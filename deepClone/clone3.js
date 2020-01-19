fba87bcd052aa863f7c81660b612f128bb3676d4
lambert1129 <lambert1129@foxmail.com>
2020-01-19 18:04:18 +0800

    style:修改函数名字

--- deepClone/clone3.js
+++ deepClone/clone3.js
@@ -1,6 +1,6 @@
 // -> 考虑value存在数组
 
-export function clone3(target) {
+export function clone(target) {
     if (typeof target === 'object') {
         let cloneTarget = Array.isArray(target) ? [] : {};
         for (const key in target) {
