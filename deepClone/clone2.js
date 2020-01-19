63322d5a8a2f9c7762d57912652d304dc0cd8cc5
lambert1129 <lambert1129@foxmail.com>
2020-01-19 18:00:26 +0800

    add:增加原生局类型返回，引用类型递归

---
+++ deepClone/clone2.js
@@ -0,0 +1,13 @@
+// ->.原生数据类型直接返回，引用类型递归
+
+export function clone2(target) {
+    if (typeof target === 'object') {
+        let cloneTarget = {};
+        for (const key in target) {
+            cloneTarget[key] = clone(target[key]);
+        }
+        return cloneTarget;
+    } else {
+        return target;
+    }
+}
