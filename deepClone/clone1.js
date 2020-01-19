a5edce5c190969cc85fd3e90c0f69fe57b1b3611
lambert1129 <lambert1129@foxmail.com>
2020-01-19 18:02:32 +0800

    style:修改函数名

--- deepClone/clone1.js
+++ deepClone/clone1.js
@@ -6,7 +6,7 @@ const target = {
     },
 }
 
-export function clone1(target) {
+export function clone(target) {
     let cloneTarget = {};
     for (const key in target) {
         cloneTarget[key] = target[key];
