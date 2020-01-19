b9d67be7bb41d586a3f2221faad8657fc15b7077
lambert1129 <lambert1129@foxmail.com>
2020-01-19 18:01:11 +0800

    add:考虑value存在数组

---
+++ deepClone/clone3.js
@@ -0,0 +1,13 @@
+// -> 考虑value存在数组
+
+export function clone3(target) {
+    if (typeof target === 'object') {
+        let cloneTarget = Array.isArray(target) ? [] : {};
+        for (const key in target) {
+            cloneTarget[key] = clone(target[key]);
+        }
+        return cloneTarget;
+    } else {
+        return target;
+    }
+}
