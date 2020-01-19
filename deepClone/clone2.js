914abf2b29f38a29ea8b2236f9bf2d3f6520612b
lambert1129 <lambert1129@foxmail.com>
2020-01-19 18:03:44 +0800

    style:修改函数名

--- deepClone/clone2.js
+++ deepClone/clone2.js
@@ -1,6 +1,6 @@
 // ->.原生数据类型直接返回，引用类型递归
 
-export function clone2(target) {
+export function clone(target) {
     if (typeof target === 'object') {
         let cloneTarget = {};
         for (const key in target) {
