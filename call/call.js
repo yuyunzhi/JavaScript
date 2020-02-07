f3991122d23e922e2e3146bae5f6701359498446
lambert1129 <lambert1129@foxmail.com>
2020-02-07 20:31:22 +0800

    add:call 函数实现

---
+++ call/call.js
@@ -0,0 +1,21 @@
+function myCall(context,...args) {
+    // 1
+    if (typeof this !== 'function'){
+        throw new TypeError('error')
+    }
+    // 2
+    context = context || window
+    // 3
+    const fn = Symbol()
+    context.fn = this
+    // 4
+    const result = context.fn(...args)
+    // 5
+    delete context.fn
+
+    return result
+}
+
+Function.prototype.myCall = myCall
+
+// getName.myCall(obj， 'str1', 'str2')
