71850b4564ff25b530a979b084dd220de02d25de
lambert1129 <lambert1129@foxmail.com>
2020-02-25 22:39:21 +0800

    add:curry function

---
+++ curry/curry.js
@@ -0,0 +1,21 @@
+function curry(fn){
+    if(fn.length <= 1) return fn
+    const generator = (...args) =>{
+        if(fn.length === args.length){
+            return fn(...args)
+        }else{
+            return (...args2)=>{
+                return generator(...args,...args2)
+            }
+        }
+    }
+    return generator
+}
+
+// 使用方式
+let add = (a,b,c,d ) => a+b+c+d
+const curriedAdd = curry(add)
+curriedAdd(5)(6)(7)(8)
+curriedAdd(5,6)(7)(8)
+curriedAdd(5,6,7,8)
+
