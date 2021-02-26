4c932cd5a5d1db634eb73a5bf3bb2b149d7acc9e
yuyunzhi <lambert1129@foxmail.com>
2021-02-26 19:16:58 +0800

    feat: add debounce

---
+++ debounce/debounce.js
@@ -0,0 +1,40 @@
+
+// js版本
+
+function debounce (fn,delay){
+  let timer
+  return (...params)=>{
+    if(timer){
+      clearTimeout(timer)
+    }
+    setTimeout(()=>{
+      fn(...params)
+    },delay)
+  }
+}
+
+// 使用方式
+const log = debounce(()=>console.log('hello world'),5000)
+
+log()
+log()
+log()
+
+// react 版本
+
+function useDebounce (value,delay=300) {
+  const [debounceValue,setDebounceValue]= useState(value)
+  useEffect(()=>{
+    const timer = setTimeout(()=>{
+      setDebounceValue(value)
+    },delay)
+    return ()=>{
+        clearTimeout(timer)
+    }
+  },[value,delay])
+  return debounceValue
+}
+// 使用方式
+const debounceValue = useDebounce(value,5000)
+
+
