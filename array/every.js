ec14192bb7db2270428a5dca8573326f1dc75d19
yuyunzhi <lambert1129@foxmail.com>
2021-03-03 15:03:54 +0800

    feat: add every

---
+++ array/every.js
@@ -0,0 +1,15 @@
+
+Array.prototype.myEvery = function (fn,context){
+  const arr = Array.prototype.slice.call(this)
+  for(let i = 0;i<arr.length;i++){
+    if(!fn.call(context,arr[i],i,arr)){
+      return false
+    }
+  }
+  return true
+}
+
+
+let result = [2, 3, 0, 1, 4].myEvery(function isBigEnough(element, index, array) {
+  return (element >= 5); //数组中是否有一个元素大于 10
+})
