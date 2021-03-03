e385d973dbdf628e3e8aa37f2232f3e8838abd6c
yuyunzhi <lambert1129@foxmail.com>
2021-03-03 14:51:08 +0800

    feat: add filter

---
+++ array/filter.js
@@ -0,0 +1,24 @@
+
+Array.prototype.myFilter = function (fn,context) {
+  const arr = Array.prototype.slice.call(this)
+  let filterArray = []
+  for(let i = 0;i<arr.length;i++){
+      fn.call(context,arr[i],i,arr) && filterArray.push(arr[i])
+  }
+}
+
+// 使用reduce 实现 filter
+Array.prototype.myFilter2 = function (fn,context){
+  const arr = Array.prototype.slice.call(this)
+  let filterArray = arr.reduce((pre,item,index)=>{
+     return fn.call(context,item,index,arr) ? [...pre,item] : [...pre]
+  },[])
+
+  return filterArray
+
+}
+
+
+[1,2,3,4,5,6].myFilter((item,index,arr)=>{
+  return  item > 2
+})
--- array/map.js
+++ array/map.js
@@ -1,17 +1,25 @@
+
+// 使用for循环实现map
 Array.prototype.myMap = function (fn, context) {
-  console.log('this',this)
   const arr = Array.prototype.slice.call(this)
-  console.log('arr',arr)
   const newArray = []
   for (let i = 0; i < arr.length; i++) {
     if(!arr.hasOwnProperty(i)) continue
     newArray.push(fn.call(context, arr[i], i, arr))
   }
-
   return newArray
 }
 
+// 使用reduce实现map
+Array.prototype.myMap2 = function (fn,context){
+  const arr = Array.prototype.slice.call(this)
+  arr.reduce((pre,cur,index)=>{
+    return [...pre,fn.call(context,cur,index,arr)]
+  },[])
+}
 
-    [1, 2, 3, 4, 5].myMap((item, index, array) => {
+
+
+[1, 2, 3, 4, 5].myMap((item, index, array) => {
   return item + 1
 })
