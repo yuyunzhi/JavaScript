c6aaaa1ed2f1e9b04a3330669fc360beeae79434
yuyunzhi <lambert1129@foxmail.com>
2021-03-10 18:52:19 +0800

    feat: update splice

---
+++ array/splice.js
@@ -0,0 +1,20 @@
+
+let arr = [1,2,3,4]
+
+arr.splice(2,1) // [1,2,4]
+
+let arr = [1,2,3,4]
+
+arr.splice(0,2) // [3,4]
+
+let arr = [1,2,3,4]
+
+arr.splice(0,2,4) //[4,3,4]
+
+let arr = [1,2,3,4]
+
+arr.splice(2) //[3,4]
+
+let arr = [1,2,3,4]
+
+arr.splice(-1) //[4]
