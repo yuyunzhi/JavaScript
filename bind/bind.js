5bd1c4b02f1408dd2c84b36ae7232379a6702e20
yuyunzhi <lambert1129@foxmail.com>
2021-10-15 08:54:52 +0800

    feat:update bind

--- bind/bind.js
+++ bind/bind.js
@@ -2,7 +2,7 @@ Function.prototype.myBind =  function (context,...rest){
   if(typeof this !== 'function'){
     throw TypeError('not a function')
   }
-  context = context | window
+  // context = context | window
   context.fn = this
 
   let newFnx = function (){
@@ -12,5 +12,14 @@ Function.prototype.myBind =  function (context,...rest){
   return newFnx
 }
 
+let obj = {
+  a:1
+}
 
+function x(args){
+  console.log(this.a);
+  return args
+}
 
+let x1  = x.myBind(obj,'hahaa')
+console.log(x1());
